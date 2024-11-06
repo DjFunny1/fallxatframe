8d58177cce2202683a4bb38cfea173d6c01bf70c
Ronald Chacon <166889445+DjFunny1@users.noreply.github.com>
2024-11-05 23:44:22 -0500

    first try

---
+++ java.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('button');
+const gif = document.getElementById('gif');
+const gif2 = document.getElementById('gif2');
+
+let isPlaying = true;
+const gifURL = 'https://cdn.pixabay.com/animation/2023/10/20/03/38/03-38-03-793_512.gif';
+const staticImageURL = '';
+
+button.addEventListener('click', () => {
+if (isPlaying) {
+    gif.src = staticImageURL;
+} else {
+    gif.src = gifURL;
+}
+if (isPlaying) {
+    gif2.src = staticImageURL;
+}else {
+    gif2.src - gifURL;
+}
+isPlaying = !isPlaying;
+});

14c5fa7aa8817630659768a3639d882a032cd340
Ronald Chacon <166889445+DjFunny1@users.noreply.github.com>
2024-11-15 00:00:42 -0500

    Update java.js

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ const gif2 = document.getElementById('gif2');
 
 let isPlaying = true;
 const gifURL = 'https://cdn.pixabay.com/animation/2023/10/20/03/38/03-38-03-793_512.gif';
-const staticImageURL = '';
+const staticImageURL = 'https://xatimg.com/image/5j2M5vNqniE3.png';
 
 button.addEventListener('click', () => {
 if (isPlaying) {
